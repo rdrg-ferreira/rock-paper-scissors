fa7eff674ac3cf1e4b66f642edf8ca7a22543edb
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2025-07-14 19:07:12 +0100

    created getHumanChoice() and moved choices array to global scope

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+const choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
     const intChoice = Math.floor((Math.random() * 3));
     
     return choices[intChoice];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose between rock, paper or scissors:");
+    
+    while (!choices.includes(choice.toLowerCase())) {
+        alert("That is not a valid option!");
+        choice = prompt("Choose between rock, paper or scissors:");
+    }
+
+    return choice.toLowerCase();
 }
